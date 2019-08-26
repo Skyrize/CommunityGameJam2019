08b18f4a0bc00dbe804678c5a593c85679c5d4ac
Choz <remy.sainsard@gmail.com>
2019-08-26 19:10:26 +0200

    enemies sprites added

---
+++ CommunityGameJam2019/Assets/BodyControllerComponents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyControllerComponents : MonoBehaviour
+{
+    [Space]
+    [Header("Animator")]
+    public Animator animatorBody;
+
+    private Vector2 direction;
+    private float speed;
+
+    private void FixedUpdate()
+    {
+        AnimatorHandler();
+    }
+
+    private void AnimatorHandler()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        speed = Mathf.Clamp(direction.magnitude, 0.0f, 1.0f);
+
+		animatorBody.SetFloat("speed", speed);
+    }
+}
---
+++ CommunityGameJam2019/Assets/Scripts/LegsControllerComponents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegsControllerComponents : MonoBehaviour
+{
+    [Space]
+    [Header("Animator")]
+    public Animator animatorLegs;
+
+    private Vector2 direction;
+    private float speed;
+
+    private void FixedUpdate()
+    {
+        AnimatorHandler();
+    }
+
+    private void AnimatorHandler()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        speed = Mathf.Clamp(direction.magnitude, 0.0f, 1.0f);
+
+		animatorLegs.SetFloat("speed", speed);
+    }
+}

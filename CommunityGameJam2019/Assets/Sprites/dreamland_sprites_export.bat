f69eaf02b33b434b0ce5f2db991e0f0cc50b1790
Choz <remy.sainsard@gmail.com>
2019-08-27 14:09:35 +0200

    [SPRITES] Some decorations sprites

---
+++ CommunityGameJam2019/Assets/Sprites/dreamland_sprites_export.bat
@@ -0,0 +1,72 @@
+set folderRoot=Dreamland
+set scaling=--scale 2
+
+rem [Player]
+
+rem idle dreamland (body/legs)
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/PlayerIdle/player_body_idle_left.aseprite %scaling% --save-as %folderRoot%/PlayerIdle/player_body_idle_left.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/PlayerIdle/player_legs_idle_left.aseprite %scaling% --save-as %folderRoot%/PlayerIdle/player_legs_idle_left.png
+
+rem walk dreamland (body/legs)
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/PlayerWalk/player_body_walk_left.aseprite %scaling% --save-as %folderRoot%/PlayerWalk/player_body_walk_left.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/PlayerWalk/player_legs_walk_left.aseprite %scaling% --save-as %folderRoot%/PlayerWalk/player_legs_walk_left.png
+
+rem walk (legs) dreamland
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/PlayerWalk/player_legs_walk_left.aseprite %scaling% --save-as %folderRoot%/PlayerWalk/player_legs_walk_left.png
+
+rem Finger dreamland
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/PlayerFinger/finger_right.aseprite %scaling% --save-as %folderRoot%/PlayerFinger/finger_right.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/PlayerFinger/finger_left.aseprite %scaling% --save-as %folderRoot%/PlayerFinger/finger_left.png
+
+rem --------------
+
+rem [Decoration]
+set folderCustom=Decorations
+
+rem Rock
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/rock_1.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/rock_1.png
+
+rem Heart
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/heart.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/heart.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/heart_blue.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/heart_blue.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/heart_green.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/heart_green.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/heart_yellow.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/heart_yellow.png
+
+rem Flover
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/flover_red_1.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/flover_red_1.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/flover_white_1.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/flover_white_1.png
+
+rem Candy
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/candy_1.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/candy_1.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/mini_candy_red.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/mini_candy_red.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/mini_candy_blue.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/mini_candy_blue.png
+
+rem Pills
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/pill_blue.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/pill_blue.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/pill_red.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/pill_red.png
+
+rem bullet heart
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/bullet_heart.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/bullet_heart.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/bullet_heart.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/bullet_heart.png
+
+
+rem --------------
+
+rem [Map]
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/Map/map.aseprite %scaling% --save-as %folderRoot%/Map/map.png
+
+rem --------------
+
+rem [Enemies]
+
+rem Sheep Idle & Walk
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/Enemies/Sheep/sheep_idle.aseprite %scaling% --save-as %folderRoot%/Enemies/Sheep/sheep_idle.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/Enemies/Sheep/sheep_walk.aseprite %scaling% --save-as %folderRoot%/Enemies/Sheep/sheep_walk.png
+
+rem Monkey Idle & Walk
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/Enemies/Monkey/monkey_idle.aseprite %scaling% --save-as %folderRoot%/Enemies/Monkey/monkey_idle.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/Enemies/Monkey/monkey_walk.aseprite %scaling% --save-as %folderRoot%/Enemies/Monkey/monkey_walk.png
+
+rem Wolf Idle & Walk
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/Enemies/Wolf/wolf_idle.aseprite %scaling% --save-as %folderRoot%/Enemies/Wolf/wolf_idle.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/Enemies/Wolf/wolf_walk.aseprite %scaling% --save-as %folderRoot%/Enemies/Wolf/wolf_walk.png
---
+++ CommunityGameJam2019/Assets/Sprites/realityland_sprites_export.bat
@@ -0,0 +1,17 @@
+set folderRoot=Dreamland
+set scaling=--scale 2
+
+rem REALITYLAND
+
+set folderRoot=Realityland
+
+rem Decoration
+
+set folderCustom=Decorations
+
+rem Blood
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/drop_of_blood.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/drop_of_blood.png
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/blood_particle.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/blood_particle.png
+
+rem Bullet
+"C:\Program Files\Aseprite\Aseprite.exe" -b %folderRoot%/%folderCustom%/bullet.aseprite %scaling% --save-as %folderRoot%/%folderCustom%/bullet.png
